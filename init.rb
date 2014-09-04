3f9d3fa0e2506a4bf831d15b36684545591e3d30
kawahiro311 <hiro.kawasaki0311@gmail.com>
2014-09-05 01:47:01 +0900

    version 1.0.0

--- init.rb
+++ init.rb
@@ -1,12 +1,14 @@
-require_dependency 'idobata/hook_listener'
-
 Redmine::Plugin.register :redmine_idobata do
   name 'Redmine Idobata plugin'
   author 'Hiro Kawasaki'
   description 'Idobata.io chat integration'
-  version '0.0.1'
+  version '1.0.0'
   url 'https://github.com/kawahiro311/redmine_idobata'
   author_url 'https://github.com/kawahiro311'
+
+  Rails.configuration.to_prepare do
+    require_dependency 'idobata/hook_listener'
+  end
 
   settings :default => {
     'idobata_webhook_url' => ''
